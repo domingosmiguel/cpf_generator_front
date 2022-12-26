30a299d511d46c3c6e1224a68dbe3447fb8575d1
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2022-12-26 00:44:34 -0300

    feat: add cnpj generation

--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
 const body = document.querySelector('#middle');
-const checkbox_label = document.getElementById('checkbox_label');
+const checkbox_label_cpf = document.getElementById('checkbox_label_cpf');
+const checkbox_label_cnpj = document.getElementById('checkbox_label_cnpj');
+const checkbox_label_mask = document.getElementById('checkbox_label_mask');
+const labels = [checkbox_label_cpf, checkbox_label_cnpj, checkbox_label_mask];
 
 const input = document.getElementById('newCPF');
-const checkbox = document.getElementById('punctuation');
+const checkbox_cpf = document.getElementById('checkbox_cpf');
+const checkbox_mask = document.getElementById('checkbox_mask');
 
 const getCPF = async () => {
   input.value = '';
   input.placeholder = 'Gerando CPF...';
   try {
     const promise = await axios.get(
-      `https://cpf-generator-api-8nmz.onrender.com/newCPF?p=${checkbox.checked}`
+      `https://cpf-generator-api-8nmz.onrender.com/${
+        checkbox_cpf.checked ? 'newCPF' : 'newCNPJ'
+      }?mask=${checkbox_mask.checked}`
     );
     input.value = promise.data;
     navigator.clipboard.writeText(promise.data);
@@ -20,11 +26,9 @@ const getCPF = async () => {
 
 document.querySelector('#light').addEventListener('click', () => {
   body.classList.replace('bg-dark', 'bg-light');
-  checkbox.classList.replace('bs-dark', 'bs-light');
-  checkbox_label.classList.replace('text-light', 'text-dark');
+  labels.forEach((obj) => obj.classList.replace('text-light', 'text-dark'));
 });
 document.querySelector('#dark').addEventListener('click', () => {
   body.classList.replace('bg-light', 'bg-dark');
-  checkbox.classList.replace('bs-light', 'bs-dark');
-  checkbox_label.classList.replace('text-dark', 'text-light');
+  labels.forEach((obj) => obj.classList.replace('text-dark', 'text-light'));
 });
